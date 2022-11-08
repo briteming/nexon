f62fd867d13a794fc42114ebd9477a2a42f650eb
Feng Kaiyu <loveress01@outlook.com>
2022-11-08 11:40:48 +0800

    feat: FKYnJYQ specification.

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -25,7 +25,6 @@ import { searchNotion } from '@/lib/search-notion'
 import { useDarkMode } from '@/lib/use-dark-mode'
 
 import { Footer } from './Footer'
-import { GitHubShareButton } from './GitHubShareButton'
 import { Loading } from './Loading'
 import { NotionPageHeader } from './NotionPageHeader'
 import { Page404 } from './Page404'
@@ -311,7 +310,6 @@ export const NotionPage: React.FC<types.PageProps> = ({
         pageTitle={tagsPage && propertyToFilterName ? title : undefined}
       />
 
-      <GitHubShareButton />
     </>
   )
 }
